00526ce3b21c0e73d147622e73def9de9111c9bb
Syeda Marium Faheem <45970754+mariumfaheem@users.noreply.github.com>
2021-01-17 16:50:01 +0500

    Add files via upload

---
+++ s1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+main(){
+   char in[75];
+   printf("\nEnter Serial Number\n");
+   scanf(%s,in)
+   if(!strcmp(in,"S123N456",8))
+    {
+       print("Serial Number is correct \n")
+     }
+
+
+}
